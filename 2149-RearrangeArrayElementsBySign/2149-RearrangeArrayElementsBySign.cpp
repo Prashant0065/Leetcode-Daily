0aa7e96a067655a1d207b7b4e3c446108bebf55e
Prashant0065 <prashantlavania9@gmail.com>
2026-09-04 15:54:56 +0530

    Create 2149-RearrangeArrayElementsBySign

---
+++ 2149-RearrangeArrayElementsBySign/2149-RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,21 @@
+// Last updated: 9/4/2026, 3:54:55 PM
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int posindex = 0;
+        int negindex = 1;
+        for(int i = 0; i<n ; i++){
+            if(nums[i] > 0){
+                ans[posindex]= nums[i];
+                posindex+=2;
+            }
+            else{
+                ans[negindex] = nums[i];
+                negindex += 2;
+            }
+        } 
+        return ans;  
+    }
+};
